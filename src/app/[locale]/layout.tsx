06765ff48ebce227c1b949c004ca72ff7ef42126
Dmitriy <dmitriy.symon@gmail.com>
2026-03-09 13:00:12 +0100

    binotel scripts removed

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -118,7 +118,6 @@ export default async function LocaleLayout({
           </CartProvider>
         </NextIntlClientProvider>
         <Toaster />
-        <BinotelScripts />
       </body>
     </html>
   );
